d1c186696c9b48861d25d4dab941d497f8f4bb21
Gerard Wesseling <gerardwesseling16@gmail.com>
2026-09-21 22:10:43 +0200

    cleanup

---
+++ src/components/Story.tsx
@@ -0,0 +1,238 @@
+"use client";
+
+import Image from "next/image";
+import styles from "./Story.module.css";
+
+export function Story() {
+  return (
+    <div style={{ height: '100%', width: '100%', display: 'flex', flexDirection: 'column' }}>
+      <div className={styles.container}>
+        <div className={styles.textWrap}>
+          <p className={styles.title}>Little Nutbrown Hare, who was going to bed, held on tight to Big Nutbrown Hare&rsquo;s very long ears.</p>
+        </div>
+
+        <div className={styles.imageWrap}>
+          <Image src="/images/1.png" alt="Test Image" width={1151} height={959} />
+        </div>
+      </div>
+
+      <div className={styles.container}>
+        <div className={styles.textWrap}>
+          <p className={styles.text}>He wanted to be sure that Big Nutbrown Hare was listening. <br /> &ldquo;Guess how much I love you,&rdquo; he said.</p>
+
+          <p className={styles.text}>&ldquo;Oh I don&rsquo;t think I could guess that,&rdquo; <br /> said Big Nutbrown Hare.</p>
+        </div>
+
+        <div className={styles.imageWrap}>
+          <Image src="/images/3.png" alt="Test Image" width={1192} height={1342} />
+        </div>
+      </div>
+
+      <div className={styles.container}>
+        <div className={styles.textWrap}>
+          <p className={styles.text}>&ldquo;This much,&rdquo; said Little Nutbrown Hare, stretching out his arms as wide they could go.</p>
+        </div>
+
+        <div className={styles.imageWrap}>
+          <Image src="/images/4.png" alt="Test Image" width={1192} height={1084} />
+        </div>
+      </div>
+
+      <div className={styles.container}>
+        <div className={styles.textWrap}>
+          <p className={styles.text}>Big Nutbrown Hare had even longer arms. &ldquo;But I love you this much,&rdquo; he said.</p>
+
+          <p className={styles.text}>Hmm, that is a lot, thought Little Nutbrown Hare.</p>
+        </div>
+
+        <div className={styles.imageWrap}>
+          <Image src="/images/5.png" alt="Test Image" width={1192} height={1405} />
+        </div>
+      </div>
+
+      <div className={styles.container}>
+        <div className={styles.textWrap}>
+          <p className={styles.text}>&ldquo;I love you as high as I can reach,&rdquo; said Little Nutbrown Hare.</p>
+        </div>
+
+        <div className={styles.imageWrap}>
+          <Image src="/images/6.png" alt="Test Image" width={936} height={670} />
+        </div>
+      </div>
+
+      <div className={styles.container}>
+        <div className={styles.textWrap}>
+          <p className={styles.text}>&ldquo;I love you as high as I can reach,&rdquo; said Big Nutbrown Hare.</p>
+        </div>
+
+        <div className={styles.imageWrap}>
+          <Image src="/images/7.png" alt="Test Image" width={1192} height={1462} />
+        </div>
+      </div>
+
+      <div className={styles.container}>
+        <div className={styles.textWrap}>
+          <p className={styles.text}>That is quite high, thought Little Nutbrown Hare. I wish I had arms like that.</p>
+        </div>
+
+        <div className={styles.imageWrap}>
+          <Image src="/images/8.png" alt="Test Image" width={1192} height={1462} />
+        </div>
+      </div>
+
+      <div className={styles.container}>
+        <div className={styles.textWrap}>
+          <p className={styles.text}>Then Little Nutbrown Hare had an good idea. He tumbled upside down and reached up the tree trunk with his feet.</p>
+        </div>
+
+        <div className={styles.imageWrap}>
+          <Image src="/images/9.png" alt="Test Image" width={912} height={1333} />
+        </div>
+      </div>
+
+      <div className={styles.container}>
+        <div className={styles.textWrap}>
+          <p className={styles.text}>&ldquo;I love you all the way up my toes!&rdquo; he said.</p>
+        </div>
+
+        <div className={styles.imageWrap}>
+          <Image src="/images/10.png" alt="Test Image" width={932} height={1435} />
+        </div>
+      </div>
+
+      <div className={styles.container}>
+        <div className={styles.textWrap}>
+          <p className={styles.text}>&ldquo;And I love you all the way up to your toes,&rdquo; said Big Nutbrown Hare, swinging him up over his head.</p>
+        </div>
+
+        <div className={styles.imageWrap}>
+          <Image src="/images/11.png" alt="Test Image" width={1167} height={1462} />
+        </div>
+      </div>
+
+      <div className={styles.container}>
+        <div className={styles.textWrap}>
+          <p className={styles.text}>&ldquo;I love you as high as I can hop!&rdquo; laughed Little Nutbrown Hare,</p>
+        </div>
+
+        <div className={styles.imageWrap}>
+          <Image src="/images/12.png" alt="Test Image" width={1099} height={1329} />
+        </div>
+      </div>
+
+      <div className={styles.container}>
+        <div className={styles.textWrap}>
+          <p className={styles.text}>bouncing up and down.</p>
+        </div>
+
+        <div className={styles.imageWrap}>
+          <Image src="/images/13.png" alt="Test Image" width={1161} height={1317} />
+        </div>
+      </div>
+
+      <div className={styles.container}>
+        <div className={styles.textWrap}>
+          <p className={styles.text}>&ldquo;But I love you as high as I can hop,&rdquo; smiled Big Nutbrown Hare -- and he hopped so high that his ears touched the branches above.</p>
+        </div>
+
+        <div className={styles.imageWrap}>
+          <Image src="/images/14.png" alt="Test Image" width={1149} height={1462} />
+        </div>
+      </div>
+
+      <div className={styles.container}>
+        <div className={styles.textWrap}>
+          <p className={styles.text}>That&apos;s good hopping, thought Little Nutbrown Hare. I wish I could hop like that.</p>
+        </div>
+
+        <div className={styles.imageWrap}>
+          <Image src="/images/15.png" alt="Test Image" width={1192} height={1462} />
+        </div>
+      </div>
+
+      <div className={styles.container}>
+        <div className={styles.textWrap}>
+          <p className={styles.text}>&ldquo;I love you all the way down the lane as far as the river,&rdquo; cried Little Nutbrown Hare.</p>
+        </div>
+
+        <div className={styles.imageWrap}>
+          <Image src="/images/16.png" alt="Test Image" width={1192} height={1090} />
+        </div>
+      </div>
+
+      <div className={styles.container}>
+        <div className={styles.textWrap}>
+          <p className={styles.text}>&ldquo;I love you across the river and over the hills&rdquo; cried Big Nutbrown Hare.</p>
+        </div>
+
+        <div className={styles.imageWrap}>
+          <Image src="/images/17.png" alt="Test Image" width={1192} height={1097} />
+        </div>
+      </div>
+
+      <div className={styles.container}>
+        <div className={styles.textWrap}>
+          <p className={styles.text}>That&apos;s very far, through Little Nutbrown Hare. He was almost too sleepy to think anymore.</p>
+        </div>
+
+        <div className={styles.imageWrap}>
+          <Image src="/images/18.png" alt="Test Image" width={632} height={353} />
+        </div>
+      </div>
+
+      <div className={styles.container}>
+        <div className={styles.textWrap}>
+          <p className={styles.text}>Then he looked beyond the thornbushes, out into the big dark night. Nothing could be farther than the sky.</p>
+        </div>
+
+        <div className={styles.imageWrap}>
+          <Image src="/images/19.png" alt="Test Image" width={654} height={363} />
+        </div>
+      </div>
+
+      <div className={styles.container}>
+        <div className={styles.textWrap}>
+          <p className={styles.text}>&ldquo;I love you right up to the moon,&rdquo; he said, and closed his eyes.</p>
+
+          <p className={styles.text}>&ldquo;Oh, that&apos;s far,&rdquo; said Big Nutbrown Hare. &ldquo;That&apos;s very, very far.&rdquo;</p>
+        </div>
+
+        <div className={styles.imageWrap}>
+          <Image src="/images/20.png" alt="Test Image" width={1154} height={1423} />
+        </div>
+      </div>
+
+      <div className={styles.container}>
+        <div className={styles.textWrap}>
+          <p className={styles.text}>Big Nutbrown Hare settled Little Nutbrown Hare into his bed of leaves.</p>
+        </div>
+
+        <div className={styles.imageWrap}>
+          <Image src="/images/21.png" alt="Test Image" width={645} height={311} />
+        </div>
+      </div>
+
+      <div className={styles.container}>
+        <div className={styles.textWrap}>
+          <p className={styles.text}>He leaned over and kissed him good night.</p>
+        </div>
+
+        <div className={styles.imageWrap}>
+          <Image src="/images/22.png" alt="Test Image" width={1192} height={1350} />
+        </div>
+      </div>
+
+      <div className={styles.container}>
+        <div className={styles.textWrap}>
+          <p className={styles.text}>Then he lay down close by and whispered wuth a smile, &ldquo;I love your right up to the moon --</p>
+
+          <p className={styles.text}>and back.&rdquo;</p>
+        </div>
+
+        <div className={styles.imageWrap}>
+          <Image src="/images/23.png" alt="Test Image" width={1184} height={1090} />
+        </div>
+      </div>
+    </div>
+  );
+}
